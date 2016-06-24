c72646f8497feadd169c3b17f292f3d6f6b83859
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-06-24 12:58:42 -0400

    Optimization branch.

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -227,7 +227,8 @@ class Flix {
         val ast = PropertyGen.collectProperties(tast)
         val sast = Simplifier.simplify(ast)
         val lifted = LambdaLift.lift(sast)
-        val numbered = VarNumbering.number(lifted)
+        val opt = Optimizer.optimize(lifted)
+        val numbered = VarNumbering.number(opt)
         val east = CreateExecutableAst.toExecutable(numbered)
         val compiled = LoadBytecode.load(this, east, options)
         Verifier.verify(compiled, options) map {
